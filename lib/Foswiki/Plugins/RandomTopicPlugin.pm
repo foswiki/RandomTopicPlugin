fae16022442f4c2c49b5d410de5e76baf0a6f92a
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-08-22 15:53:36 +0000

    Item11058: modernized
       * use registerTagHandler instead of commonTagsHandler
       * delay init, i.e. snarfing in the list of all topics only when needed
       * initialize global variables properly to make this run in persistent perl environments
       * expand standard tokens in format strings
       * add header, footer and separator format strings
       * remove traces of twiki
       * fix MANIFEST so that it works with pseudo-install
       * format lists using 3 spaces, not tabs
    
    
    
    
    git-svn-id: http://svn.foswiki.org/trunk/RandomTopicPlugin@12359 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/RandomTopicPlugin.pm
+++ lib/Foswiki/Plugins/RandomTopicPlugin.pm
@@ -18,7 +18,7 @@ package Foswiki::Plugins::RandomTopicPlugin;
 use strict;
 
 use vars qw(
-            $VERSION $RELEASE @topicList $defaultIncludes $defaultExcludes
+            $VERSION $RELEASE @topicList $defaultIncludes $defaultExcludes $doneInit
     );
 
 # This should always be $Rev$ so that Foswiki can determine the checked-in
@@ -29,67 +29,71 @@ $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '20100328';
+$RELEASE = '2.0';
 
 
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    $defaultIncludes = Foswiki::Func::getPreferencesValue( "RANDOMTOPICPLUGIN_INCLUDE" );
-    $defaultExcludes = Foswiki::Func::getPreferencesValue( "RANDOMTOPICPLUGIN_EXCLUDE" );
-
-    @topicList = Foswiki::Func::getTopicList( $web );
-
+    Foswiki::Func::registerTagHandler('RANDOMTOPIC', \&handleRandomTopic);
+    $doneInit = 0;
     return 1;
 }
 
-sub handleRandomPage {
-    my $attr = shift;
+sub init {
+    my $session = shift;
 
-    my $format;
-    my $topics = 1;
-    my $includes;
-    my $excludes;
+    return if $doneInit;
 
-    $format =
-      Foswiki::Func::extractNameValuePair( $attr, "format" ) ||
-          "\$t* \$topic\$n";
-    $topics =
-      Foswiki::Func::extractNameValuePair( $attr, "topics" ) || 1;
+    $doneInit = 1;
+    @topicList = Foswiki::Func::getTopicList( $session->{webName} );
+    $defaultIncludes = Foswiki::Func::getPreferencesValue( "RANDOMTOPICPLUGIN_INCLUDE" );
+    $defaultExcludes = Foswiki::Func::getPreferencesValue( "RANDOMTOPICPLUGIN_EXCLUDE" );
 
-    $includes =
-      Foswiki::Func::extractNameValuePair( $attr, "include" ) ||
-          $defaultIncludes || "^.+\$";
+}
 
-    $excludes =
-      Foswiki::Func::extractNameValuePair( $attr, "exclude" ) ||
-          $defaultExcludes || "^\$";
+sub handleRandomTopic {
+    my ($session, $params, $topic, $web) = @_;
+
+    init($session);
+
+    my $header = $params->{header} || '';
+
+    my $format = $params->{format};
+    $format = '$topic' unless defined $format;
+
+    my $separator = $params->{separator} || '';
+    my $footer = $params->{footer} || '';
+    my $topics = $params->{topics} || 1;
+
+    my $includes = $params->{include};
+    $includes = ($defaultIncludes || "^.+\$") unless defined $includes;
+    
+    my $excludes = $params->{exclude};
+    $excludes = ($defaultExcludes || "^\$") unless defined $excludes;
 
     my @pickFrom = grep { /$includes/ && !/$excludes/ } @topicList;
 
-    my $result = "";
+    my @result = ();
     my %chosen = ();
-    my $pickable = scalar( @pickFrom );
+    my $nrTopics = scalar( @pickFrom );
+    my $pickable = $nrTopics;
     while ( $topics && $pickable ) {
-        my $i = int( rand( scalar @pickFrom ));
+        my $i = int( rand( $nrTopics ));
         unless ( $chosen{$i} ) {
             my $line = $format;
             $line =~ s/\$topic/$pickFrom[$i]/g;
-            $line =~ s/\$t/\t/g;
-            $line =~ s/\$n/\n/g;
-            $result .= $line;
+            push(@result, $line);
             $topics--;
             $pickable--;
             $chosen{$i} = 1;
         }
     }
-    return $result;
-}
 
-sub commonTagsHandler {
-### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
-    $_[0] =~ s/%RANDOMTOPIC%/&handleRandomPage()/ge;
-    $_[0] =~ s/%RANDOMTOPIC{(.*?)}%/&handleRandomPage($1)/ge;
+    return '' unless @result;
+    my $result = $header.join($separator, @result).$footer;
+
+    return Foswiki::Func::decodeFormatTokens($result);
 }
 
 1;
--- lib/Foswiki/Plugins/RandomTopicPlugin/build.pl
+++ lib/Foswiki/Plugins/RandomTopicPlugin/build.pl
@@ -1,9 +1,4 @@
 #!/usr/bin/perl -w
-#
-# Read the comments at the top of lib/TWiki/Plugins/Build.pm for
-# details of how the build process works, and what files you
-# have to provide and where.
-#
 BEGIN {
     foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
         unshift @INC, $pc;
